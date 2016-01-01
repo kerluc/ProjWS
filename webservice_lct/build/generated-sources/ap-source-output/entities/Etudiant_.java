d49b0ed3baf6336669caf91f4ea36283ce01a84d
Wael <wael54@gmail.com>
2016-01-01 05:57:29 +0100

    added rapport, gitignore, xhtml

---
+++ webservice_lct/build/generated-sources/ap-source-output/entities/Etudiant_.java
@@ -0,0 +1,18 @@
+package entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-24T02:29:13")
+@StaticMetamodel(Etudiant.class)
+public class Etudiant_ { 
+
+    public static volatile SingularAttribute<Etudiant, String> pw;
+    public static volatile SingularAttribute<Etudiant, Long> id;
+    public static volatile SingularAttribute<Etudiant, String> nom;
+    public static volatile SingularAttribute<Etudiant, String> prenom;
+    public static volatile SingularAttribute<Etudiant, String> email;
+    public static volatile SingularAttribute<Etudiant, Integer> budget;
+
+}
